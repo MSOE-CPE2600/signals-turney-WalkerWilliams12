e00672e33920ef51e5806bf9bf1318e5b228a84d
Walker Williams <williamsw@msoe.edu>
2025-11-10 21:17:21 -0600

    Part 4 implemented

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,61 @@
 /**
- * File: 
- * Modified by:
+ * @file rev_signal.c
+ * @brief Recives a signal from the user and prints the integer value that belongs to it
  * 
- * Brief summary of program:
+ * Course: CPE 2600
+ * Section: 111
+ * Assignment: Lab 10 
+ * Name: Walker Williams
+ * 
+ * Compile: gcc -o recv_signal recv_signal.c
+ *          ./recv_signal
  */
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * @brief Signal handler for SIGUSR1 that receives additional data
+ * @param sig - signal number
+ * @param info - signal information
+ * @param input - unused input 
+ */
+void handle_sigusr1(int sig, siginfo_t *info, void *input) 
+{
+    (void)input; // Unused 
+    
+    if (info != NULL) 
+    {
+        printf("Received SIGUSR1 from process %d with value: %d\n", info->si_pid, info->si_value.sival_int);
+    } 
+    else 
+    {
+        printf("Received SIGUSR1\n");
+    }
+}
+
+int main() {
+    struct sigaction sa;
+    
+    printf("Receiver PID: %d\n", getpid());
+    printf("Waiting for SIGUSR1 signal...\n");
+    
+    // Signal handler for SA_SIGINFO flag
+    sa.sa_sigaction = handle_sigusr1;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;
+    
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("sigaction");
+        return 1;
+    }
+    
+    // Infinite loop to keep program running
+    while(1) {
+        pause(); // Wait for signals
+    }
+    
+    return 0;
+}
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,51 @@
 /**
- * File: 
- * Modified by:
+ * @file send_signal.c
+ * @brief Sends a SIGUSER1 signal with a random integer value to a process
  * 
- * Brief summary of program:
+ * Course: CPE 2600
+ * Section: 111
+ * Assignment: Lab 10 
+ * Name: Walker Williams
+ * 
+ * Compile: gcc -o send_signal send_signal.c
+ *          ./send_signal
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) 
+{
+    if (argc != 2) 
+    {
+        printf("Usage: %s <target_pid>\n", argv[0]);
+        printf("Example: %s 1234\n", argv[0]);
+        return 1;
+    }
+    
+    pid_t target_pid = atoi(argv[1]);
+    union sigval value;
+    
+    // Seed random number generator
+    srand((unsigned int)time(NULL));
+    
+    // Generates random number between 1 and 100
+    int random_num = (rand() % 100) + 1;
+    value.sival_int = random_num;
+    
+    printf("Sending SIGUSR1 to process %d with value: %d\n", target_pid, random_num);
+    
+    // Sends signal with data
+    if (sigqueue(target_pid, SIGUSR1, value) == -1) 
+    {
+        perror("sigqueue");
+        return 1;
+    }
+    
+    printf("Signal sent successfully!\n");
+    
+    return 0;
+}
