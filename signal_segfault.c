b4de2593259b327e4851b41232bd7b9774bb250c
Walker Williams <williamsw@msoe.edu>
2025-11-10 21:12:05 -0600

    signal_segfault.c implemented

--- signal_segfault.c
+++ signal_segfault.c
@@ -1,24 +1,44 @@
 /**
  * @file signal_segfault.c
- * @brief Program that dereferences a null pointer causing a segmentation fault
- */
-
-/**
- * Modified by:
+ * @brief Signal handler for SIGSEGV and prints message for segmntation fault
  * 
- * Brief summary of modifications:
+ * Course: CPE 2600
+ * Section: 111
+ * Assignment: Lab 10 
+ * Name: Walker Williams
+ * 
+ * Compile: gcc -o signal_segfault signal_segfault.c
+ *          ./signal_segfault
  */
 
 
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+/**
+ * @brief Signal handler for SIGSEGV
+ * @param sig Signal number
+ */
+void handle_sigsegv(int sig) {
+    printf("Received SIGSEGV signal - segmentation fault detected!\n");
+    printf("But continuing execution... will fault again!\n");
+}
 
 int main (int argc, char* argv[]) {
+    // Register signal handler for SIGSEGV
+    signal(SIGSEGV, handle_sigsegv);
+    
+    printf("Program started - will cause segmentation fault\n");
+    
     // Declare a null pointer
     int* i = NULL;
 
-    // Dereference the null pointer
+    // Dereference the null pointer - this will cause SIGSEGV
     printf("The value of i is: %d\n", *i);
 
-    // Return to exit the program
+    // This line will never be reached due to infinite faults - test line to make sure it never gets here
+    printf("This line will never be printed\n");
+    
     return 0;
 }
