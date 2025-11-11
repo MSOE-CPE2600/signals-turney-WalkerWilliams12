8fef12d9d2970034600c75e640f520e95dbcf9d1
Walker Williams <williamsw@msoe.edu>
2025-11-10 21:09:34 -0600

    signal_handler.c implemented

--- signal_handler.c
+++ signal_handler.c
@@ -1,12 +1,14 @@
 /**
  * @file signal_handler.c
- * @brief Sets a signal handler for SIGINT, the handler prints a message and then quits
- */
-
-/**
- * Modified by:
+ * @brief Signal handler for SIGINT and prints a mesage
  * 
- * Brief summary of modifications:
+ * Course: CPE 2600
+ * Section: 111
+ * Assignment: Lab 10 
+ * Name: Walker Williams
+ * 
+ * Compile: gcc -o signal_handler signal_handler.c
+ *          ./signal_handler
  */
 
 
@@ -15,23 +17,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static int signal_count = 0;
+
 /**
- * @brief Signal handler for SIGINT - prints a message and exits
+ * @brief Signal handler for SIGINT - prints a message but does NOT exit
  */
-void handle_signal() {
-    printf("Received a signal\n");
-    exit(1);
+void handle_signal() 
+{
+    signal_count++;
+    printf("Received SIGINT signal #%d\n", signal_count);
+    printf("Use 'kill -9 %d' to terminate this process\n", getpid());
 }
 
-int main() {
+int main() 
+{
+    printf("Process PID: %d\n", getpid());
+    printf("SIGINT handler - process will not exit on CTRL+C\n");
+    printf("Use 'kill -9 %d' to terminate this process\n", getpid());
 
     // Register for the signal
     signal(SIGINT, handle_signal);
 
     // Wait until a signal is received
-    while(1) {
-        printf("Sleeping\n");
-        sleep(1);
+    while(1) 
+    {
+        printf("Sleeping - Received %d SIGINT signals so far\n", signal_count);
+        sleep(3);
     }
 
     return 0;
